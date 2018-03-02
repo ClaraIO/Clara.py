3047f435679b90b8d138ad18f092675e40d8b0a0
Capuccino <chinodesuuu@gmail.com>
2018-03-02 06:37:44 +0000

    I fucked up on submodules again

--- src/handler/k3/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-"""k3 is a Python library that provides command handling for use in bots."""
-
-version_info = (3, 0, 0, "a", "Ferrilata")
-version = '{0}.{1}.{2}{3} "{4}"'.format(*version_info)
